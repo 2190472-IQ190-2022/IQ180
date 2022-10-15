a3a13d0f62f5774c3bae15e74ee7737802989566
Krittapasa <kritapassk138@gmail.com>
2022-10-15 18:18:40 +0700

    Implement update_score implment function new_round that is called by update_score and function update_score itself

--- game.py
+++ game.py
@@ -27,9 +27,29 @@ class Game:
     def reset():
         #reset the game, scores and etc.
         pass
+    def new_round():
+        p1_cleared=False
+        p2_cleared=False
+        p1_played=False
+        p2_played=False
+        game_time_p1=0
+        game_time_p2=0
     def update_score():
         #Define the winner of current round by comparing boolean cleared and game_time, then update score and player's turn
-        pass
+        if p1_cleared and p2_cleared:
+            if game_time_p1 < game_time_p2:
+                p1_score+=1
+                turn=1
+            else if game_time_p1 > game_time_p2:
+                p2_score+=1
+                turn=2
+        else if p1_cleared:
+            p1_score+=1
+            turn=1
+        else if p2_cleared:
+            p2_score+=1
+            turn=2
+        new_round()
     def game_started():
         #Random first player in the first game (And maybe other purpose)
         pass
