6f08b2dd65fd1bb0d2a5fea93121e606c2a380f3
CaQtiml <lsivakorn@gmail.com>
2022-10-17 11:52:16 +0700

    update ip address for testing

--- mock_server.py
+++ mock_server.py
@@ -4,7 +4,7 @@ import pickle
 from time import time
 from game import Game
 
-server = "192.168.1.53"
+server = "10.202.134.107"
 port = 5555
 
 def threaded_client(conn, player, gameId, games):
@@ -23,9 +23,9 @@ def threaded_client(conn, player, gameId, games):
                 if not data:
                     break
                 elif type(data) == str:    
-                    if data == "reset":
-                        game.resetWent()
-                    elif data == "get_equation":
+                    # if data == "reset":
+                    #     game.resetWent()
+                    if data == "get_equation":
                         game.generate_question()
                     elif data != "get":
                         game.check(player, data)
