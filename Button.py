c44ee9bfa0fba4e25ec79e83fe4bd8fd0ab512f3
SupakornSJB <limit4break@gmail.com>
2022-10-17 15:24:08 +0700

    implement better algorithm for clicking and hovering button

--- Button.py
+++ Button.py
@@ -15,6 +15,7 @@ class Button:
             size - button's size (tuple of two int e.g. (width, height))
             enabled_color - button's color when it's clickable (tuple of three int e.g. (R, G, B))
             hover_color - button's color when the mouse hover over the button (tuple of three int e.g. (R, G, B))
+        pressed_color - button's color when the mouse presses the button (tuple of three int e.g. (R, G, B))
             disabled_color - button's color when the button's disabled (tuple of three int e.g. (R, G, B))
             text - text on the button (string)
             disabled - Whether the button is disabled, if button is disabled, you can't click and it will not do the assigned operation
@@ -22,6 +23,7 @@ class Button:
             img - pygame image object that are used when the button is ENABLED (from pygame.image.load(<Image Path>))
             img_hover - pygame image object that are used when the button is HOVERED (from pygame.image.load(<Image Path>))
             img_pressed - pygame image object that are used when the button is PRESSED (from pygame.image.load(<Image Path>))
+            img_disabled - pygame image object that are used whent the button is DISABLED (from pygame.image.load(<Image Path>))
             operation - name of function that you wants to be called when pressed
                     (just used name of the function with out the bracket, e.g. operation=test_func)
             any other parameter name that are not listed above can be enter to be used as "operation" function parameter
@@ -29,25 +31,27 @@ class Button:
     """
 
     def __init__(self, window, button_font, pos=(300, 300), size=(100, 100), enabled_color=(100, 100, 100),
-                 hover_color=(170, 170, 170), disabled_color=(50, 50, 50), text="text", disabled=False,
-                 img=None, img_hover=None, img_pressed=None, operation=None, ** op_args):
+                 hover_color=(170, 170, 170), pressed_color = (20, 20, 20), disabled_color=(50, 50, 50), text="text",
+                 disabled=False, img=None, img_hover=None, img_pressed=None, img_disabled=None, operation=None, ** op_args):
         self.window = window
         self.button_font = button_font
         self.pos = pos
         self.size = size
+        self.mouse = pygame.mouse
         self.enabled_color = enabled_color
         self.hover_color = hover_color
         self.disabled_color = disabled_color
+        self.pressed_color = pressed_color
         self.text = text
         self.clicked = False
+        self.status = 0 # 0 is disbled 1 is enabled 2 is hovered 3 is pressed
         if operation is None:
-            self.disabled = True
+            self.status = 0
         else:
-            self.disabled = False
+            self.status = 1
             self.operation = operation
         if disabled:
-            self.disabled = True
-        print(op_args)
+            self.disabled = 0
 
     def set_pos(self, pos):
         self.pos = pos
@@ -55,49 +59,70 @@ class Button:
     def set_text(self, text):
         self.text = text
 
-    def is_hovering(self, mouse_pos):
+    def update_button(self):
+        """
+            This function control all the hover and press interaction of the EACH button
+            Put this function on the MAIN LOOP, NOT EVENT LOOP
+        """
+        # status = 0
+        if self.status == 0:
+            return None
+        # status = 1 not press and not hover
+        if not self.is_hovering() and not self.is_pressed():
+            self.status = 1
+        # status = 2 not press and hover
+        if self.is_hovering() and not self.is_pressed():
+            self.status = 2
+        # status = 3 press and hover
+        if self.is_hovering() and self.is_pressed() and self.status != 3:
+            self.status = 3
+            self.operation()
+        elif self.status == 3 and not self.is_hovering() and not self.is_pressed():
+            self.status = 1
+        elif self.status == 3 and self.is_hovering() and not self.is_pressed():
+            self.status = 2
+
+        self.draw()
+
+    def is_hovering(self):
         """
             You can use this function to return the BOOLEAN that tells whether the mouse hover the button or not
-            :param mouse_pos: mouse position, recommend using pygame.mouse.get_pos()
         """
-        if self.pos[0] < mouse_pos[0] < self.pos[0]+self.size[0] and self.pos[1] < mouse_pos[1] < self.pos[1]+self.size[1] and not self.disabled:
+        if self.pos[0] < self.mouse.get_pos()[0] < self.pos[0]+self.size[0] and self.pos[1] < self.mouse.get_pos()[1] < self.pos[1]+self.size[1] and self.status != 0:
             return True
         return False
 
-    def draw(self, mouse):
+    def is_pressed(self):
+        if self.mouse.get_pressed()[0]:
+            return True
+        return False
+
+    def draw(self):
         """
             call this function to draw THIS ONE BUTTON OBJECT ONLY, drawing multiple button is outside the capability of this function.
-            Recommend: you can implement function to loop over all button on your screen, then call this draw() on each button
-            :param mouse: mouse position, recommend using pygame.mouse.get_pos()
         """
         color = self.enabled_color
-        if self.is_hovering(mouse):
+
+        if self.status == 2:
             color = self.hover_color
 
-        if self.disabled:
+        if self.status == 0:
             color = self.disabled_color
+
+        # if the button is pressed, draw button
+        if self.status == 3:
+            color = self.pressed_color
 
         pygame.draw.rect(self.window, color, [self.pos[0], self.pos[1], self.size[0], self.size[1]])
         self.text_rendered = self.button_font.render(self.text, 1, (0, 0, 0))
         self.window.blit(self.text_rendered, (self.pos[0]+self.size[0]/2-self.text_rendered.get_width()/2,
                 self.pos[1]+self.size[1]/2-self.text_rendered.get_height()/2))
 
-    def press(self, mouse):
-        """
-            This function should be called when mouse is pressed
-            recommened: use if event.type == MOUSEBUTTONDOWN then press this button
-            this function checks if mouse is hovering, if it is, call operation()
-            :param mouse: mouse position, recommend using pygame.mouse.get_pos()
-        """
-        if is_hovering(mouse):
-            if not self.disabled:
-                self.operation()
-
     def set_operation(self, operation):
         self.operation = operation
 
     def disable_button(self):
-        self.disabled = True
+        self.status = 0
 
     def enable_button(self):
-        self.disabled = False
+        self.status = 1
--- test.py
+++ test.py
@@ -1,13 +1,38 @@
 # print("For testing purpose")
 # print(eval("(2+4)*6/2")==18)
 
-from game import Game
+import pygame
+import Button
 
-g1 = Game(1, "MS TEAM", "Taiwan")
-arr, equation, sum = g1.generate_question()
-print("-------------")
-print(arr)
-print(equation)
-print(sum)
-print("-------------")
-print(g1.check(equation, sum+2))
+WHITE = (0, 0, 0)
+
+# from game import Game
+#
+# g1 = Game(1, "MS TEAM", "Taiwan")
+# arr, equation, sum = g1.generate_question()
+# print("-------------")
+# print(arr)
+# print(equation)
+# print(sum)
+# print("-------------")
+# print(g1.check(equation, sum+2))
+
+def test_func():
+    print("hello func")
+
+pygame.font.init()
+WIN = pygame.display.set_mode((800, 600))
+BUTTON_FONT1 = pygame.font.SysFont('arial', 69)
+running = True
+b1 = Button.Button(WIN, BUTTON_FONT1, operation=test_func)
+while running:
+    WIN.fill(WHITE)
+    mouse = pygame.mouse.get_pos()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    b1.update_button()
+    # b1.draw(mouse)
+    # print(mouse)
+    # print(b1.is_hovering(mouse))
+    pygame.display.update()
