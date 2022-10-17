ad2ae5db51d0aee1876fe269ba35889179a18a15
CaQtiml <lsivakorn@gmail.com>
2022-10-17 21:20:45 +0700

    format code to pep8

--- mock_client.py
+++ mock_client.py
@@ -6,12 +6,13 @@ from mock_network import Network
 
 clientNumber = 0
 
+
 def main():
     net = Network()
     # game = net.recv()
     print("you are p"+str(net.player))
     while True:
-        game=net.recv()
+        game = net.recv()
         print(game.turn)
         if game.p1_played and game.p2_played:
             game.update_score()
@@ -21,18 +22,19 @@ def main():
             print(game.sum)
             print(game.p1_score)
             print(game.p2_score)
-            equation_str=input("")
+            equation_str = input("")
             if net.player == 1:
-                game.p1_played=True
-                game.p1_cleared=game.check(equation_str)
+                game.p1_played = True
+                game.p1_cleared = game.check(equation_str)
             else:
-                game.p2_played=True
-                game.p2_cleared=game.check(equation_str)
+                game.p2_played = True
+                game.p2_cleared = game.check(equation_str)
             net.client.send(pickle.dumps(game))
             print("send")
         else:
             print("waiting for your turn")
         time.sleep(1)
-            
+
+
 if __name__ == "__main__":
     main()
