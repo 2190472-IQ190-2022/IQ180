13b4cbe004cc072b114bf7f9ea47008df8a19cbe
SupakornSJB <limit4break@gmail.com>
2022-10-18 15:08:25 +0700

    add number button and implement button position logic

--- mock_UI.py
+++ mock_UI.py
@@ -3,14 +3,20 @@
 
 import pygame
 from Button import Button
+from game import Game
 import random
+import math
 
 # Constant
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 1280
+HEIGHT = 720
 FPS = 60
+BUTTON_BORDER_FACTOR = 0.45 # how much button would take up the screen
+GAME_BUTTON_YPOS = 400
+GAME_BUTTON_INLINE_SPACING = 10
+GAME_BUTTON_TWOLINE_SPACING = 20
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.font.init()
 DEFAULT_FONT = pygame.font.SysFont('comicsans', 40)
@@ -21,29 +27,22 @@ menu_status = 1
 # menu status = 1 is mm1, = 2 is mm2, = 3 is game, = 4 is htp, = 5 is setting
 all_button = []
 
-def draw_main_menu_1():
+def draw_everything(current_menu_status):
     WIN.fill(WHITE)
-    this_text = DEFAULT_FONT.render("MM1", 1, BLACK)
-    WIN.blit(this_text, (WIDTH/2-this_text.get_width()/2, HEIGHT/2-this_text.get_height()/2))
-
-def draw_main_menu_2():
-    WIN.fill(WHITE)
-    this_text = DEFAULT_FONT.render("MM2", 1, BLACK)
-    WIN.blit(this_text, (WIDTH/2-this_text.get_width()/2, HEIGHT/2-this_text.get_height()/2))
-
-def draw_setting():
-    WIN.fill(WHITE)
-    this_text = DEFAULT_FONT.render("Setting", 1, BLACK)
-    WIN.blit(this_text, (WIDTH/2-this_text.get_width()/2, HEIGHT/2-this_text.get_height()/2))
-
-def draw_how_to_play():
-    WIN.fill(WHITE)
-    this_text = DEFAULT_FONT.render("How to play", 1, BLACK)
-    WIN.blit(this_text, (WIDTH/2-this_text.get_width()/2, HEIGHT/2-this_text.get_height()/2))
-
-def draw_game_window():
-    WIN.fill(WHITE)
-    this_text = DEFAULT_FONT.render("Game", 1, BLACK)
+    text_print = ""
+    if current_menu_status == 1:
+        text_print = "Main menu 1"
+    elif current_menu_status == 2:
+        text_print = "Main menu 2"
+    elif current_menu_status == 3:
+        text_print = "Game"
+    elif current_menu_status == 4:
+        text_print = "How to play"
+    elif current_menu_status == 5:
+        text_print = "Setting"
+    else:
+        text_print = "What"
+    this_text = DEFAULT_FONT.render(text_print, 1, BLACK)
     WIN.blit(this_text, (WIDTH/2-this_text.get_width()/2, HEIGHT/2-this_text.get_height()/2))
 
 def change_game_status(new_status):
@@ -60,31 +59,52 @@ def change_game_status(new_status):
                                 operation=change_game_status, new_status=3)
         all_button.append(to_game_button)
     elif new_status == 3:
-        numbers = randomize_five_number([0, 0, 0, 0, 0]) # simulate number array from game object
-        create_operation_button()
+        create_game_button()
     elif new_status == 4:
         pass
     elif new_status == 5:
         pass
-    pygame.time.wait(300) # This function was there to prevent mouse double clicking button
+    pygame.time.wait(200) # This function was there to prevent mouse double clicking button
 
 def test_func(text_test):
     print(f"this is test func {text_test}")
+
+def button_control():
+    """This function would take care of all the button"""
+    pass
 
 def randomize_five_number(array):
     length = len(array)
     for i in range(0, length, 1):
         array[i] = random.randint(0,9)
 
-def create_operation_button():
-    position_x = 100
-    for char in "+-*/()":
-        button = Button(WIN, DEFAULT_FONT, text=char, operation=test_func, text_test=char, pos=(position_x, 200))
-        all_button.append(button)
-        position_x = position_x + 100
+def calculate_button_position(number_of_button):
+    space_used = BUTTON_BORDER_FACTOR * WIDTH
+    button_size = (space_used - ((number_of_button - 1) * GAME_BUTTON_INLINE_SPACING)) / number_of_button
+    begining_position = (WIDTH - space_used) / 2
+    return math.floor(button_size), math.floor(begining_position)
 
-def update_operation_button():
-    pass
+def create_game_button():
+    numbers = [0, 0, 0, 0, 0]
+    position_y = GAME_BUTTON_YPOS
+    randomize_five_number(numbers)
+    button_size, begining_position = calculate_button_position(5)
+    position_x = begining_position
+    for i in numbers:
+        button = Button(WIN, DEFAULT_FONT, text=str(i), operation=test_func, text_test=str(i),
+                        pos=(position_x, position_y), size=(button_size, button_size))
+        all_button.append(button)
+        position_x = position_x + button_size + GAME_BUTTON_INLINE_SPACING
+
+    position_y = position_y + button_size + 20
+    button_size, begining_position = calculate_button_position(6)
+    position_x = begining_position
+
+    for char in "+-*/()":
+        button = Button(WIN, DEFAULT_FONT, text=char, operation=test_func, text_test=char,
+                        pos=(position_x, position_y), size=(button_size, button_size))
+        all_button.append(button)
+        position_x = position_x + button_size + GAME_BUTTON_INLINE_SPACING
 
 def main():
     clock = pygame.time.Clock()
@@ -99,23 +119,14 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        if menu_status == 1:
-            draw_main_menu_1()
-        elif menu_status == 2:
-            draw_main_menu_2()
-        elif menu_status == 3:
-            draw_game_window()
-        elif menu_status == 4:
-            draw_how_to_play()
-        elif menu_status == 5:
-            draw_setting()
+        draw_everything(menu_status)
 
         for button in all_button:
             button.update_button()
 
         pygame.display.update()
-        print(f"all_button len: {len(all_button)}")
-        print(f"menu_status: {menu_status}")
+        # print(f"all_button len: {len(all_button)}")
+        # print(f"menu_status: {menu_status}")
 
 
 if __name__ == "__main__":
