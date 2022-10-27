03a3251be582284a7e534e871462cd53802a06cc
Krittapasa <kritapassk138@gmail.com>
2022-10-27 12:48:47 +0700

    Updated game.py Should be the final version of game.py

--- game.py
+++ game.py
@@ -7,6 +7,9 @@ class Game:
         self.p2_score = 0
         self.p1_name = p1_name
         self.p2_name = p2_name
+        self.current_time = 0
+        self.ready = False
+        self.dummy = True
         self.turn = random.getrandbits(1)+1  # 1 for player 1's turn and 2 for player 2's turn
         self.id = id
         # for timer
@@ -23,8 +26,7 @@ class Game:
         self.p1_played = False
         self.p2_played = False
         # number array and answer for the question
-        self.numbers_array, _,self.sum = self.generate_question()
-        
+        self.numbers_array, self.equation, self.sum = self.generate_question()
 
     def generate_question(self):
         # generate each digits and answer
@@ -45,17 +47,18 @@ class Game:
                 if i != number_of_digits-1:
                     equation += random.choice(operation)
             sum = eval(equation)
+            if have_divide == 1:
+                for i in range(9,0,-1):
+                    if sum%i == 0:
+                        sum /= i
+                        sum = int(sum)
+                        numbers_array.append(i)
+                        equation = f"({equation})/{str(i)}"
+                        break
             if sum >= 10:
                 break
 
-        if have_divide == 1:
-            for i in range(9,0,-1):
-                if sum%i == 0:
-                    sum /= i
-                    sum = int(sum)
-                    numbers_array.append(i)
-                    equation = f"({equation})/{str(i)}"
-                    break
+
         # For Debugging Purpose. May remove when submitting to ajarn
         # print(equation)
         # print(eval(equation))
@@ -63,11 +66,16 @@ class Game:
         # print(numbers_array)
         # End of Debugging section
         self.sum = sum
+        random.shuffle(numbers_array)
+        self.numbers_array=numbers_array
         return numbers_array, equation, sum
 
     def check(self, string_equation):
         # Check the answer of the equation
-        return eval(string_equation) == self.sum
+        try:
+            return eval(string_equation) == self.sum
+        except:
+            return False
 
     def reset(self):
         # reset the game, scores and etc.
@@ -84,16 +92,17 @@ class Game:
         self.p2_played = False
         self.p1_game_time = 0
         self.p2_game_time = 0
-        self.numbers_array, _,self.sum = self.generate_question()
+        self.startTime = time()
+        self.numbers_array, self.equation,self.sum = self.generate_question()
         
 
     def update_score(self):
         # Define the winner of current round by comparing boolean cleared and game_time, then update score and player's turn
         if self.p1_cleared and self.p2_cleared:
-            if self.p1_game_time < self.p2_game_time:
+            if self.p1_game_time > self.p2_game_time:
                 self.p1_score += 1
                 self.turn = 1
-            elif self.p1_game_time > self.p2_game_time:
+            elif self.p1_game_time < self.p2_game_time:
                 self.p2_score += 1
                 self.turn = 2
         elif self.p1_cleared:
