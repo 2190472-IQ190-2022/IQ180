9ab2b8944bd1f076bcfb0965a3bdf009a22a9d7c
Krittapasa <kritapassk138@gmail.com>
2022-11-17 11:10:51 +0700

    Update export game & etc. -File name is in the format of Game_date_time_id -change self.file to file

--- file_operation.py
+++ file_operation.py
@@ -1,5 +1,6 @@
 import os
 import json
+from datetime import datetime
 class file_operation:
     
     def __init__(self):
@@ -13,33 +14,55 @@ class file_operation:
             pass
         #Load settings.json
         try:
-            self.file = open("settings.json")
-            self.settings = json.load(self.file)
+            file = open("settings.json")
+            self.settings = json.load(file)
             #print(self.settings)
         except:
-            self.file = open("settings.json",'x',)
+            file = open("settings.json",'x')
+            file.close()
+            file = open("settings.json",'w')
             self.settings = dict()
+            self.settings["player_name"] = ""
+            self.settings["WIDTH"] = 1280
+            self.settings["HEIGHT"] = 720
+            self.settings["game_full_screen"] = True
+            self.settings["popup_enable"] = True
+            self.settings["music_on"] = True
+            json.dump(self.settings,file,indent=4)
             #print("Created settings.json")
         os.system( "attrib +h settings.json" ) #hidden setting file
         os.chmod("settings.json",0o666)
         #print(oct(os.stat("settings.json").st_mode))
         #print(os.access("settings.json",os.X_OK))
-        self.file.close()
+        file.close()
 
     #Take dictionary
     def save_settings(self, settings):
         self.settings = settings
         try:
             os.system( "attrib -h settings.json" )
-            self.file = open("settings.json",'w')
+            file = open("settings.json",'w')
         except:
             #Just in case user manually delete the file
-            self.file = open("settings.json",'x')
-            self.file.close()
-            self.file = open("settings.json",'w')
+            file = open("settings.json",'x')
+            file.close()
+            file = open("settings.json",'w')
         json.dump(settings,self.file,indent=4)
-        self.file.close()
+        file.close()
         os.system( "attrib +h settings.json" )
 
-    def export_game(self):
-        pass
+    def export_game(self, game_data, game_id):
+        filename = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"Exported_Games")
+        path +=f"\\Game_{filename}"
+        if str(game_id) != '0':
+            path+=f"({game_id})"
+        path+=".json"
+        try:
+            file = open(path, 'x')
+            file.close()
+            file = open(path, 'w')
+        except:
+            file = open(path, 'w')
+        json.dump(game_data,file,indent=4)
+        file.close
