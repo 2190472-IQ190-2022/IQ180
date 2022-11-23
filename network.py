9c162f74e2ff1f9c22088972b4238987e67b0457
Russ Choocharn <r49062@gmail.com>
2022-11-23 16:12:37 +0700

    hard code ip

--- network.py
+++ network.py
@@ -6,7 +6,7 @@ class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.hostname=socket.gethostname()   
-        self.server = socket.gethostbyname(self.hostname)
+        self.server = "172.20.10.2"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.player = self.connect() # player 1 or 2
